ebea156f86587d31a2c4bf99972173a0df12c63c
iamlegend <iamlegendarium@gmail.com>
2024-06-28 05:26:12 +0100

    made some changes

--- client/index.js
+++ client/index.js
@@ -1,14 +1,15 @@
-const clickBtn = () =>{
-    let url = 'https://combine-4nvv.onrender.com/register'
+const clickBtn = () => {
+  let url = "https://combine-4nvv.onrender.com/register";
 
-    fetch(url)
-    .then((response)=>{
-        return response
+  fetch(url)
+    .then((response) => {
+      return response;
     })
-    .then((data)=>{
-        return data.json()
+    .then((data) => {
+      window.location.href = "/form/login.html";
+      //   return data.json();
     })
-    .catch((err)=>{
-        console.log(err);
-    })
-}
+    .catch((err) => {
+      console.log(err);
+    });
+};
